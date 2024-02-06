728b475e6003e18c058873eba92a49710da75149
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-02-06 18:19:33 -0300

    add note-cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import logoNlw from "./assets/Logo.svg";
+import { NewNoteCard } from "./components/NewNoteCard";
+import { NoteCard } from "./components/NoteCard";
+
 export function App() {
   return (
-    <div>
-      <h1>Hello Word!</h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logoNlw} alt="Logo NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
     </div>
   );
 }
